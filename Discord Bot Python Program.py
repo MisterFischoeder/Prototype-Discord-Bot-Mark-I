246026cc7303eacc0c2907daf03685d5cf080848
Sydney <qrsydney@gmail.com>
2019-10-05 20:20:45 -0400

    Some minor changes and an example for you!
    0. importing commands functionality
    1. made it possible for you to either mention the bot or use the prefix ! for commands
    2. minor spelling edit for "client"
    3. here's a very simple example of a command, which you can use by saying !ping on discord
    4. minor newline edit after @client.event

--- Discord Bot Python Program.py
+++ Discord Bot Python Program.py
@@ -1,12 +1,26 @@
 import discord
-client = discord.Client()
-@cliet.event
+# importing commands functionality
+from discord.ext import commands
+
+# made it possible for you to either mention the bot or use the prefix ! for commands
+client = commands.Bot(commands.when_mentioned_or('!'))
+
+# minor spelling edit for "client"
+@client.event
 async def on_ready():
 	print("The bot is ready!")
 	await client.change_prescence(game=discord.Game(name="Making a bot"))
 	
-@client.eventasync def on_message(message):
-	if message.author ==client.user:
+# here's a very simple example of a command, which you can use by saying !ping on discord
+@client.command()
+async def ping(ctx):
+	await ctx.send('Pong!')
+
+# minor newline edit after @client.event
+@client.event
+async def on_message(message):
+	#changed the check to ignore all bots
+	if message.author.bot:
 		return
 	if message.content == "Hello":
 		await client.send_message(message.channel, "World")
